309211f84916dabd879e565f1fc7d3b4179f601e
AkhmadievALbert <a.akhmadiev@tinkoff.ru>
2022-06-22 01:55:29 +0300

    Added comments to Server

--- albagram/src/main/scala/Server.scala
+++ albagram/src/main/scala/Server.scala
@@ -1,15 +1,16 @@
 import cats.effect._
 import config._
 import domain.authentification.Authentification
+import domain.comment.CommentService
 import domain.posts.{PostService, PostValidationInterpreter}
 import domain.themes.{ThemeService, ThemeValidationInterpreter}
 import domain.users.{UserService, UserValidationInterpreter}
 import doobie.util.ExecutionContexts
+import infrastructure.endpoint.comment.CommentEndpoints
 import infrastructure.endpoint.post.PostEndpoints
 import infrastructure.endpoint.theme.ThemeEndpoints
 import infrastructure.endpoint.user.UserEndpoints
-import infrastructure.repository.post.DoobiePostRepositoryInterpreter
-import infrastructure.repository.{DoobieAuthRepositoryInterpreter, DoobieThemeRepositoryInterpreter, DoobieUserRepositoryInterpreter}
+import infrastructure.repository.{DoobieAuthRepositoryInterpreter, DoobieCommentRepositoryInterpreter, DoobiePostRepositoryInterpreter, DoobieThemeRepositoryInterpreter, DoobieUserRepositoryInterpreter}
 import io.circe.config.parser
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
@@ -19,7 +20,7 @@ import tsec.mac.jca.HMACSHA256
 import tsec.passwordhashers.jca.BCrypt
 
 object Server extends IOApp {
-  def createServer[F[_]: ContextShift: ConcurrentEffect: Timer]: Resource[F, H4Server[F]] =
+  def createServer[F[_] : ContextShift : ConcurrentEffect : Timer]: Resource[F, H4Server[F]] =
     for {
       conf <- Resource.eval(parser.decodePathF[F, AlbagramConfig]("albagram"))
       serverEc <- ExecutionContexts.cachedThreadPool[F]
@@ -31,16 +32,18 @@ object Server extends IOApp {
       userRepo = DoobieUserRepositoryInterpreter[F](xa)
       themeRepo = DoobieThemeRepositoryInterpreter[F](xa)
       postRepo = DoobiePostRepositoryInterpreter[F](xa)
-//      bookRepo = DoobieBookRepositoryInterpreter[F](xa)
+      commentRepo = DoobieCommentRepositoryInterpreter[F](xa)
+      //      bookRepo = DoobieBookRepositoryInterpreter[F](xa)
       userValidation = UserValidationInterpreter[F](userRepo)
       themeValidation = ThemeValidationInterpreter[F](themeRepo)
       postValidation = PostValidationInterpreter[F](postRepo)
-//      bookValidation = BookValidationInterpreter[F](bookRepo)
+      //      bookValidation = BookValidationInterpreter[F](bookRepo)
       userService = UserService[F](userRepo, userValidation)
       themeService = ThemeService[F](themeRepo, themeValidation)
       postService = PostService[F](postRepo, postValidation)
+      commentService = CommentService(commentRepo, postValidation)
 
-//      bookService = BookService[F](bookRepo, bookValidation)
+      //      bookService = BookService[F](bookRepo, bookValidation)
       authenticator = Authentification.jwtAuthenticator[F, HMACSHA256](key, authRepo, userRepo)
       routeAuth = SecuredRequestHandler(authenticator)
       httpApp = Router(
@@ -49,8 +52,10 @@ object Server extends IOApp {
         "/themes" -> ThemeEndpoints
           .endpoints[F, HMACSHA256](themeService, routeAuth),
         "/posts" -> PostEndpoints
-          .endpoints[F, HMACSHA256](postService, routeAuth)
-//        "/books" -> BookEndpoints.endpoints[F, HMACSHA256](bookService, routeAuth)
+          .endpoints[F, HMACSHA256](postService, routeAuth),
+        "/comments" -> CommentEndpoints
+          .endpoints[F, HMACSHA256](commentService, routeAuth)
+        //        "/books" -> BookEndpoints.endpoints[F, HMACSHA256](bookService, routeAuth)
       ).orNotFound
       _ <- Resource.eval(DatabaseConfig.initializeDb(conf.db))
       server <- BlazeServerBuilder[F](serverEc)
